780470bbf677e40c1c92e125fbde3207ef251219
MR-KARPIN <39997022+MR-KARPIN@users.noreply.github.com>
2023-10-09 21:20:17 +0200

    Small changes

--- web-news/src/app/app-routing.module.ts
+++ web-news/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { ArticleEditFormComponent } from './article-edit-form/article-edit-form.component';
 import { ArticleCreationFormComponent } from './article-creation-form/article-creation-form.component';
-
+import { MainpageComponent } from './mainpage/mainpage.component';
 const routes: Routes = [
   {path: 'login', component: LoginFormComponent}, 
   {path: 'edit', component: ArticleEditFormComponent}, 
-  {path: 'create', component: ArticleCreationFormComponent}
+  {path: 'create', component: ArticleCreationFormComponent},
+  {path: 'main', component: MainpageComponent} 
 ];
 
 @NgModule({
